ab52868902a9408eb797866a55fa9e7707865999
Take Haru <sj0030rx@gmail.com>
2016-02-06 14:50:30 +0900

    first commit

---
+++ count3/count3/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  count3
+//
+//  Created by Takeuchi Haruki on 2016/02/06.
+//  Copyright © 2016年 Takeuchi Haruki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet var label: UILabel!
+    var number: Int=0
+    
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+    }
+    
+
+}
+
